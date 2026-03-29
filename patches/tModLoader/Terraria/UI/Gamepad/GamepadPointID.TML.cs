5f226d1c2325be0bd516d53a6a8af7dbf4b1380f
Rijam <rijam35@gmail.com>
2026-03-28 22:26:50 -0600

    Fix and support 32 Town NPC buttons

---
+++ patches/tModLoader/Terraria/UI/Gamepad/GamepadPointID.TML.cs
@@ -0,0 +1,42 @@
+namespace Terraria.UI.Gamepad;
+
+public static partial class GamepadPointID
+{
+	// public const int NPCHousing0 = 600; // Update Note: Changed from 600-650 to 600-698 for ModNPC compatibility.
+
+	/*
+	public const int NPCChat0 = 2500;
+	public const int NPCChat1 = 2501;
+	public const int NPCChat2 = 2502;
+	public const int NPCChat3 = 2503;
+	public const int NPCChat4 = 2504;
+	public const int NPCChat5 = 2505;
+	public const int NPCChat6 = 2506;
+	public const int NPCChat7 = 2507;
+	public const int NPCChat8 = 2508;
+	public const int NPCChat9 = 2509;
+	public const int NPCChat10 = 2510;
+	public const int NPCChat11 = 2511;
+	*/
+	public const int NPCChat12 = 2512;
+	public const int NPCChat13 = 2513;
+	public const int NPCChat14 = 2514;
+	public const int NPCChat15 = 2515;
+	public const int NPCChat16 = 2516;
+	public const int NPCChat17 = 2517;
+	public const int NPCChat18 = 2518;
+	public const int NPCChat19 = 2519;
+	public const int NPCChat20 = 2520;
+	public const int NPCChat21 = 2521;
+	public const int NPCChat22 = 2522;
+	public const int NPCChat23 = 2523;
+	public const int NPCChat24 = 2524;
+	public const int NPCChat25 = 2525;
+	public const int NPCChat26 = 2526;
+	public const int NPCChat27 = 2527;
+	public const int NPCChat28 = 2528;
+	public const int NPCChat29 = 2529;
+	public const int NPCChat30 = 2530;
+	public const int NPCChat31 = 2531;
+	// Could go all the way up to 2599
+}
